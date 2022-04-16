01bf1ac6e6c0241ec6766fcc5c18b3b0a314511b
AdeshMask <adeshmaske255@gmail.com>
2022-04-16 14:29:55 +0530

    update

--- src/main/java/CabInvoiceGenerator.java
+++ src/main/java/CabInvoiceGenerator.java
@@ -4,30 +4,31 @@ public class CabInvoiceGenerator {
     static final double COST_PER_KILOMETER = 10;
     static final double COST_PER_MINUTE = 1;
     static Scanner scanner = new Scanner(System.in);
-    static double distance;
-    static double time;
     static double result;
+    static final double MINIMUM_FARE = 5;
 
     public static void main(String[] args) {
-//        double distance = 2;
-//        double time = 5;
-        System.out.println("Enter number of rides:");
-        int rides = scanner.nextInt();
-        for (int i = 1;i <= rides; i++){
-            System.out.print("Enter distanc fot "+ i+" Ride");
-            distance = scanner.nextDouble();
-            System.out.print("Enter Time");
-            time = scanner.nextDouble();
-            CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
-            result = result + cabInvoiceGenerator.invoiceGenerator(distance,time);
-        }
-        double aggrigate = result / rides;
-        System.out.println("Aggrigate: "+aggrigate);
+        double distance1 = 2;
+        double time1 = 5;
+        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+        System.out.println(cabInvoiceGenerator.invoiceGenerator(distance1,time1));
+        Rides[] rides = {new Rides(2.0,5),
+        new Rides(0.1,1)};
+        System.out.println(cabInvoiceGenerator.multipleInvoiceGenerator(rides));
+    }
+    double invoiceGenerator(double distance, double time) {
+        double fare = COST_PER_KILOMETER * distance + time * COST_PER_MINUTE;
+        if (fare < MINIMUM_FARE)
+            return MINIMUM_FARE;
+        else
+            return fare;
     }
 
-    private double invoiceGenerator(double distance, double time) {
-        double fare = COST_PER_KILOMETER * distance + time * COST_PER_MINUTE;
+    double multipleInvoiceGenerator(Rides[] rides){
+        double fare = 0.0;
+        for (Rides rides1: rides) {
+            fare = COST_PER_KILOMETER * rides1.distance + rides1.time * COST_PER_MINUTE;
+        }
         return fare;
     }
-
 }
